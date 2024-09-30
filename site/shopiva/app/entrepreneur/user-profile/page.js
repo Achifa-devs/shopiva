c91dde2b23737f78bdfcfd0c810de02e01e22b52
Achifa-dev <achifa.io.llc@gmail.com>
2024-09-30 20:36:48 +0100

    ff

--- site/shopiva/app/entrepreneur/user-profile/page.js
+++ site/shopiva/app/entrepreneur/user-profile/page.js
@@ -71,8 +71,8 @@ function Genenral() {
 
     return(
         <>
-            <div className='description'>
-                <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+            <div classN className='description'ame='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
                     <p><b>Details</b></p>
                     <small>View and updateyour personal details</small>
                 </section>
@@ -152,8 +152,8 @@ function Genenral() {
 
             <hr />
 
-            <div className='description'>
-                <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+            <div classN className='description'ame='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
                     <p><b>Stores</b></p>
                     <small>
                         View and access stores connected to your Shopify account.
@@ -169,8 +169,8 @@ function Genenral() {
             <hr />
 
  
-            <div className='description'>
-                <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+            <div classN className='description'ame='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
                     <p><b>Preferred language</b></p>
                     <small>
                         When you're logged in to Shopify, this is the language you will see. It doesn't affect the language your customers see on your online store.
@@ -195,8 +195,8 @@ function Genenral() {
             <hr />
 
 
-            <div className='description'>
-                <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+            <div classN className='description'ame='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
                     <p><b>Timezone</b></p>
                 </section>
                 <div className="content-cnt">
@@ -221,148 +221,142 @@ function Security() {
     
     return(
         <>
-            <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
-                <p><b>Passkeys</b></p>
-                <p>
+            <div className='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+                    <p><b>Passkeys</b></p>
+                    <p>
+                        <small>
+                        Log in with your fingerprint, face recognition or a PIN instead of a password. Passkeys can be synced across devices logged into the same platform (like Apple ID or a Google account).
+                        </small>
+                    </p>
+                    <p><small>Learn More About Passkeys</small></p>
+                </section>
+                <div className="content-cnt" style={{height: '100px', padding: '10px'}}>
+                
+                    <section style={{height: 'auto', width: '100%', justifyContent: 'flex-start'}}>
+                        <div htmlFor="">Creating a passkey takes under a minute.</div>
+                        {/* <br /> */}
+                        <button style={{width: 'auto', borderRadius: '2.5px', marginTop: '10px'}}>Create A Passkey</button>
+                    </section>
+                
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+                    <p><b>Password</b></p>
                     <small>
-                    Log in with your fingerprint, face recognition or a PIN instead of a password. Passkeys can be synced across devices logged into the same platform (like Apple ID or a Google account).
+                        {/* View and access stores connected to your Shopify account. */}
                     </small>
-                </p>
-                <p><small>Learn More About Passkeys</small></p>
-            </section>
-
-            <div className="content-cnt">
-                
-                <section>
-                    <div className="input-cnt">
-                        <label htmlFor="">Creating a passkey takes under a minute.</label>
-                        <br />
-                        <Link href={''}>Create A Passkey</Link>
-                    </div>
-
                 </section>
-
-                
+                <div className='content-cnt'>
+                    <section style={{height: 'fit-content', width: '100%', justifyContent: 'flex-start'}}>
+                        <div>You have not set a password on your account.</div>
+                            {/* <br /> */}
+                        <button style={{width: 'auto', borderRadius: '2.5px', marginTop: '10px'}}>Create Password</button>
+                    </section>
+                </div>
             </div>
 
-            <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
-                <p><b>Password</b></p>
-
-                <small>
-                    {/* View and access stores connected to your Shopify account. */}
-                </small>
-            </section>
-
-            <div className='content-cnt'>
-                <section>
-                    <div className="input-cnt">
-                        <label htmlFor="">You have not set a password on your account.</label>
-                        <br />
-                        <Link href={''}>Create A Passkey</Link>
-                    </div>
-
-                </section>
-            </div>
+            <hr />
  
-            <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
-                <p><b>Secondary email</b></p>
-
-                <small>
-                A secondary email can be used to restore access to your account. Security notifications are also sent to this email.
-                </small>
-            </section>
-            <div className="content-cnt">
-                <section>
-                    <div className="input-cnt">
-                        <label htmlFor="">You do not have a secondary email.</label>
-                        <Link href={''}></Link>
-                    </div>
-                    
+            <div className='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+                    <p><b>Secondary email</b></p>
+                    <small>
+                    A secondary email can be used to restore access to your account. Security notifications are also sent to this email.
+                    </small>
                 </section>
+                <div className="content-cnt" style={{height: '50px'}}>
+                    <section style={{height: '20px', justifyContent: 'flex-start', width: '100%'}}>
+                        <div htmlFor="">You do not have a secondary email.</div>
+
+                    </section>
+                </div>
             </div>
 
+            <hr />
 
-            <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
-                <p><b>Two-step authentication</b></p>
-                <p><small>Learn more about two-step authentication</small></p>
-            </section>
-            <div className="content-cnt">
-                <section>
-                    <div className="input-cnt">
-                        <label htmlFor=""><h6>
-                        Authentication methods
-                        </h6></label>
-                        <p><small>After entering your password, verify your identity with an authentication method.</small></p>
-                    </div>
-                    <br />
-                    <hr />
-                    <br />
-                    <div>
-                        <h6>How it works</h6>
-                        <p>When you log in to Shopify, you’ll need to:</p>
-                        <ol>
-                            <li><small>Enter your email and password</small></li>
-                            <li><small>Complete a second step to prove that it"s you logging in. You can enter a verification code, use a security key, or confirm your login on a trusted device.</small></li>
-                        </ol>
-
-
-                        <button>
-                            Turn on two-step
-                        </button>
-                    </div>
-
-
+            <div className='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+                    <p><b>Two-step authentication</b></p>
+                    <p><small>Learn more about two-step authentication</small></p>
                 </section>
+                <div className="content-cnt">
+                    <section >
+                        <div className="input-cnt" style={{width: '100%', marginBottom: '50px'}}>
+                            <label htmlFor=""><h6>
+                            Authentication methods
+                            </h6></label>
+                            <p><small>After entering your password, verify your identity with an authentication method.</small></p>
+                        </div>
+                       
+                       
+                        <div>
+                            <h6>How it works</h6>
+                            <p>When you log in to Shopify, you’ll need to:</p>
+                            <ol>
+                                <li><small>Enter your email and password</small></li>
+                                <li><small>Complete a second step to prove that it"s you logging in. You can enter a verification code, use a security key, or confirm your login on a trusted device.</small></li>
+                            </ol>
+                            <button>
+                                Turn on two-step
+                            </button>
+                        </div>
+                    </section>
+                </div>
             </div>
 
-            <section className='description' style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
-                <p><b>Devices</b></p>
-                <p><small>You're currently logged in to Shopify on these devices. If you don't recognize a device, log out to keep your account secure.</small></p>
-            </section>
+            <hr />
 
-            <div className="content-cnt" >
-                <section>
-                    <h6>Logged In</h6>
-
-                    <hr />
-
-                    <div style={{width: '100%', background: '#fff', height: '60px', borderBottom: '1px solid #000'}} className='input-cnt'>
-                        <div className="left" style={{width: 'auto',float: 'left'}}>
-                            <div style={{height: 'auto'}}>
-                                <span></span>
-                                <span><small>Chrome On Windows</small></span>
-                                &nbsp;
-                                &nbsp;
-                                <span><small>This Device</small></span>
-                            </div>
-                            <div style={{height: 'auto'}}></div>
-                            <div style={{height: 'auto'}}><small>Abuja(FCT), Nigeria</small></div>
-                        </div>
-                        <div className="right" style={{width: 'auto',float: 'right'}}>
-                            <button style={{height: '30px', borderRadius: '5px'}}>Log out</button>
-                        </div>
-                    </div>
-                    <div style={{width: '100%', background: '#fff', height: '60px', borderBottom: '1px solid #000'}} className='input-cnt'>
-                        <div className="left" style={{width: 'auto',float: 'left'}}>
-                            <div style={{height: 'auto'}}>
-                                <span></span>
-                                <span><small>Chrome On Windows</small></span>
-                                &nbsp;
-                                &nbsp;
-                                <span><small>This Device</small></span>
-                            </div>
-                            <div style={{height: 'auto'}}></div>
-                            <div style={{height: 'auto'}}><small>Abuja(FCT), Nigeria</small></div>
-                        </div>
-                        <div className="right" style={{width: 'auto',float: 'right'}}>
-                            <button style={{height: '30px', borderRadius: '5px'}}>Log out</button>
-                        </div>
-                    </div>
-
-                    
-                    <br />
-                    
+            <div className='description'>
+                <section style={{padding: '10px 10px ', minHeight: '50px', display: 'flex', alignItem: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+                    <p><b>Devices</b></p>
+                    <p><small>You're currently logged in to Shopify on these devices. If you don't recognize a device, log out to keep your account secure.</small></p>
                 </section>
+                <div className="content-cnt" >
+                    <section>
+                        <h6 style={{width: '100%'}}>Logged In</h6>
+                        <hr />
+                        <div style={{width: '100%', background: '#fff', height: '60px', borderBottom: '1px solid #000'}} className='input-cnt'>
+                            <div className="left" style={{width: 'auto',float: 'left'}}>
+                                <div style={{height: 'auto'}}>
+                                    <span></span>
+                                    <span><small>Chrome On Windows</small></span>
+                                    &nbsp;
+                                    &nbsp;
+                                    <span><small>This Device</small></span>
+                                </div>
+                                <div style={{height: 'auto'}}></div>
+                                <div style={{height: 'auto'}}><small>Abuja(FCT), Nigeria</small></div>
+                            </div>
+                            <div className="right" style={{width: 'auto',float: 'right'}}>
+                                <button style={{height: '30px', borderRadius: '5px'}}>Log out</button>
+                            </div>
+                        </div>
+                        <div style={{width: '100%', background: '#fff', height: '60px', borderBottom: '1px solid #000'}} className='input-cnt'>
+                            <div className="left" style={{width: 'auto',float: 'left'}}>
+                                <div style={{height: 'auto'}}>
+                                    <span></span>
+                                    <span><small>Chrome On Windows</small></span>
+                                    &nbsp;
+                                    &nbsp;
+                                    <span><small>This Device</small></span>
+                                </div>
+                                <div style={{height: 'auto'}}></div>
+                                <div style={{height: 'auto'}}><small>Abuja(FCT), Nigeria</small></div>
+                            </div>
+                            <div className="right" style={{width: 'auto',float: 'right'}}>
+                                <button style={{height: '30px', borderRadius: '5px'}}>Log out</button>
+                            </div>
+                        </div>
+                
+                        <br />
+                
+                    </section>
+                </div>
             </div>
 
         </>
