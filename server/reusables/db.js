9ebbbbc73ceaf186559b236f7fd04d7f934983fe
Achifa-dev <achifa.io.llc@gmail.com>
2024-10-11 22:56:59 +0100

    jj

--- server/reusables/db.js
+++ server/reusables/db.js
@@ -1,6 +1,6 @@
 const {Client,Pool} = require('pg');
-// let  DATABASE_URL  = `postgresql://achifa.io.llc:cflV8XEbCO7h@ep-billowing-sunset-28191429-pooler.us-east-2.aws.neon.tech/shopiva?sslmode=require`;
-let  DATABASE_URL  = `postgresql://postgres:asdfghjkl@localhost:5432/shopiva`;
+let  DATABASE_URL  = `postgresql://achifa.io.llc:cflV8XEbCO7h@ep-billowing-sunset-28191429-pooler.us-east-2.aws.neon.tech/shopiva?sslmode=require`;
+// let  DATABASE_URL  = `postgresql://postgres:asdfghjkl@localhost:5432/shopiva`;
 require('dotenv').config();    
 
 
