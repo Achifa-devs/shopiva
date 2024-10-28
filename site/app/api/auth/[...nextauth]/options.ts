20bb5e5ff20a5be1de4cf3b19da0df707baa30d9
Achifa-dev <achifa.io.llc@gmail.com>
2024-10-28 07:01:10 +0100

    hh

--- site/app/api/auth/[...nextauth]/options.ts
+++ site/app/api/auth/[...nextauth]/options.ts
@@ -8,8 +8,8 @@ import FacebookProvider from "next-auth/providers/facebook";
 export const options: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.AUTH_GOOGLE_ID as string,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET as string
+            clientId: process.env.NEXT_PUBLIC_AUTH_GOOGLE_ID as string,
+            clientSecret: process.env.NEXT_PUBLIC_AUTH_GOOGLE_SECRET as string
         })
     ]
 }
